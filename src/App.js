70963703efb476154ec23cb69db56b07b482b759
yoelwildanto <yoel.wildantop@gmail.com>
2023-11-20 11:06:58 +0700

    body manage product

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import AdminPage from './pages/AdminView';
 import HeaderManageProduct from './components/manageProduct/headerManageProduct';
+import BodyManageProduct from './components/manageProduct/bodyManageProduct';
 import "./App.css"
 import { Route, Routes } from 'react-router-dom';
 
@@ -9,7 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path='/' element={<AdminPage />} />
-        <Route path='/manage-product' element={<AdminPage viewHeader={<HeaderManageProduct />} />} />
+        <Route path='/manage-product' element={<AdminPage viewHeader={<HeaderManageProduct/>} viewBody={<BodyManageProduct/>} />} />
       </Routes>
     </div>
   );
