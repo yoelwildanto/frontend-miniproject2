44213e3caefa17926f8b2efdd167a1966ccc6027
Yoel Wildanto <142624911+yoelwildanto@users.noreply.github.com>
2023-11-20 11:17:21 +0700

    Scrum 15 (#2)
    * update
    
    product in json data. admin view. sidebar
    
    * up
    
    * add product
    
    * switch
    
    * body manage product
    
    * body manage product
    
    * up

---
+++ src/components/manageProduct/bodyManageProduct.jsx
@@ -0,0 +1,232 @@
+import { useDisclosure, Box, Image } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+import { Checkbox, CheckboxGroup } from "@chakra-ui/react";
+import { Switch, Button } from "@chakra-ui/react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
+import ModalUpdateProduct from "../modal/updateProduct";
+
+const BodyManageProduct = ({}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectAll, setSelectAll] = useState(false);
+  const [product, setProduct] = useState([]);
+  const [productById, setProductById] = useState(null);
+  const [productId, setProductId] = useState(null);
+  const [productStatus, setProductStatus] = useState(null);
+  const [productDelete, setProductDelete] = useState(null);
+  const toast = useToast();
+
+  const getProductAll = async () => {
+    try {
+      const res = await axios.get(`http://localhost:8080/product`);
+      setProduct(res?.data?.data);
+      console.log(res?.data?.data)
+    } catch (err) {
+      toast({
+        title: err?.response?.data,
+        status: "error",
+        position: "top",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const updateProductStatus = async (productId, newStatus) => {
+    try {
+      const res = await axios.patch(
+        `http://localhost:8080/product/update/${productId}`,
+        {
+          statusId: newStatus,
+        }
+      );
+      setProductStatus(res?.data?.data);
+      toast({
+        title: res?.data?.message,
+        status: "success",
+        position: "top",
+        duration: 2000,
+        isClosable: true,
+      });
+    } catch (err) {
+      toast({
+        title: err?.response?.data,
+        status: "error",
+        position: "top",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const deleteProduct = async (productId) => {
+    try {
+      const res = await axios.delete(
+        `http://localhost:8080/product/product/${productId}`
+      );
+      setProductDelete(res?.data?.data);
+      toast({
+        title: res?.data?.message,
+        status: "success",
+        position: "top",
+        duration: 2000,
+        isClosable: true,
+      });
+    } catch (err) {
+      toast({
+        title: err?.response?.data,
+        status: "error",
+        position: "top",
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+  };
+
+  useEffect(() => {
+    getProductAll();
+  }, [productStatus, productDelete]);
+
+  const handleModalClose = () => {
+    onClose();
+    getProductAll();
+  };
+
+  return (
+    <Box>
+      <TableContainer>
+        <Table size="sm" variant="striped" bgColor="#FFF6F3">
+          <Thead bgColor="#132043" h="40px">
+            <Tr>
+              <Th>
+                {/* <Checkbox
+                  isChecked={selectAll}
+                  onChange={() => setSelectAll(!selectAll)}
+                /> */}
+              </Th>
+              <Th fontSize="16px" color="#8FB381">
+                Product Image
+              </Th>
+              <Th fontSize="16px" color="#8FB381">
+                Product Name
+              </Th>
+              <Th fontSize="16px" color="#8FB381">
+                Price
+              </Th>
+              <Th fontSize="16px" color="#8FB381">
+                Stock
+              </Th>
+              <Th fontSize="16px" color="#8FB381">
+                Category
+              </Th>
+              <Th fontSize="16px" color="#8FB381">
+                Description
+              </Th>
+              <Th fontSize="16px" color="#8FB381" textAlign="center">
+                Status
+              </Th>
+              <Th fontSize="16px" color="#8FB381" textAlign="center">
+                Action
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody
+            color="#1E1E1E"
+            fontFamily="Poppins"
+            fontWeight="500"
+            fontSize="20px"
+          >
+            {product?.map((item,index) => (
+              <Tr>
+                <Td>
+                  {/* <Checkbox isChecked={selectAll} /> */}
+                </Td>
+                <Td key={index}>
+                  <Image
+                    w="100px"
+                    h="60px"
+                    src={`${process.env.REACT_APP_IMAGE_URL}/product/${
+                      item?.image
+                    }`}
+                  />
+                </Td>
+                <Td fontSize="16px">{item?.productName}</Td>
+                <Td fontSize="16px">Rp {item?.price}</Td>
+                <Td fontSize="16px">{item?.stock}</Td>
+                <Td fontSize="16px">{item?.category?.categoryName}</Td>
+                <Td fontSize="16px">
+                  <Box
+                    w="160px"
+                    h="50px"
+                    overflow="hidden"
+                    textOverflow="ellipsis"
+                    whiteSpace="normal"
+                  >
+                    {item?.description}
+                  </Box>
+                </Td>
+                <Td textAlign="center">
+                  <Switch
+                    colorScheme="green"
+                    isChecked={item.statusId === 2}
+                    onChange={() =>
+                      updateProductStatus(item.id, item.statusId === 2 ? 1 : 2)
+                    }
+                  />
+                </Td>
+                <Td textAlign="center">
+                  <Box display="flex" justifyContent="center" gap="10px">
+                    <Button
+                      size="sm"
+                      w="50px"
+                      bgColor="#132043"
+                      color="#ffffff"
+                      onClick={() => {
+                        setProductById(item);
+                        setProductId(item?.id);
+                        onOpen();
+                      }}
+                    >
+                      Edit
+                    </Button>
+                    <Button
+                      size="sm"
+                      w="50px"
+                      variant="outline"
+                      color="#132043"
+                      border="1px solid #132043"
+                      onClick={() => deleteProduct(item.id)}
+                    >
+                      Delete
+                    </Button>
+                  </Box>
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      {isOpen && (
+        <ModalUpdateProduct
+          isOpen={isOpen}
+          onClose={handleModalClose}
+          productId={productId}
+          productById={productById}
+        />
+      )}
+    </Box>
+  );
+};
+
+export default BodyManageProduct;
---
+++ src/components/modal/createProduct/index.jsx
@@ -0,0 +1,167 @@
+import { useDisclosure, InputLeftElement, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton } from '@chakra-ui/react';
+import { Box, Button, Input, InputGroup, InputLeftAddon, Textarea, Select, Stack } from '@chakra-ui/react'
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText } from '@chakra-ui/react'
+import React, { useRef, useEffect, useState } from 'react';
+import { IconPhoto, IconPhotoDown } from '@tabler/icons-react';
+import { useDropzone } from "react-dropzone";
+import { useFormik } from "formik";
+import { useToast } from '@chakra-ui/react';
+import axios from 'axios';
+
+const ModalCreateProduct = ({ isOpen, onClose }) => {
+    const [image, setImage] = useState(null);
+    const [category, setCategory] = useState([]);
+    const [status, setStatus] = useState([]);
+    const toast = useToast();
+
+    const getCategoryAll = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8080/category`);
+            setCategory(res?.data?.data);
+        } catch (err) {
+            toast({ title: err?.response?.data, status: 'error', position: 'top', duration: 2000, isClosable: true });
+        }
+    };
+
+    const getStatusAll = async () => {
+        try {
+            const res = await axios.get(`http://localhost:8080/status`);
+            setStatus(res?.data?.data);
+        } catch (err) {
+            toast({ title: err?.response?.data, status: 'error', position: 'top', duration: 2000, isClosable: true });
+        }
+    };
+
+    useEffect(() => {
+        getCategoryAll();
+        getStatusAll();
+    }, []);
+
+    const handleResetForm = () => {
+        formik.resetForm();
+        setImage(null);
+    };
+
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+        accept: ['image/*'],
+        onDrop: (acceptedFiles) => {
+            if (acceptedFiles.length > 0) {
+                const imageURL = URL.createObjectURL(acceptedFiles[0]);
+                setImage(imageURL);
+            }
+        },
+    });
+
+    const createProduct = async (productName, categoryId, price, stock, description, statusId) => {
+        try {
+            let formData = new FormData();
+            formData.append("productName", productName);
+            formData.append("categoryId", categoryId);
+            formData.append("price", price);
+            formData.append("stock", stock);
+            formData.append("description", description);
+            formData.append("statusId", statusId);
+            acceptedFiles.forEach((file) => {
+                formData.append("image", file);
+            });
+            const res = await axios.post("http://localhost:8080/product", formData);
+            toast({ title: res?.data?.message, status: 'success', position: 'top', duration: 4000, isClosable: true });
+            setTimeout(() => { onClose(); window.location.reload(); }, 3000);
+        } catch (err) {
+            toast({ title: err?.response?.data, status: 'error', position: 'top', duration: 2000, isClosable: true });
+        }
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            productName: "",
+            categoryId: "",
+            price: "",
+            stock: "",
+            description: "",
+            statusId: "",
+        },
+
+        onSubmit: (values) => {
+            createProduct(values.productName, values.categoryId, values.price, values.stock, values.description, values.statusId);
+        },
+    });
+
+    return (
+        <Box w="900px">
+            <Modal onClose={() => { onClose(); window.location.reload(); }} isOpen={isOpen} size='custom' isCentered>
+                <ModalOverlay />
+                <form onSubmit={formik.handleSubmit} >
+                    <ModalContent w="1050px" h="700px" bg={"#132043"}>
+                        <ModalHeader color={'white'}>Create Product</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Box display='flex' justifyContent='center' flexDirection='column' gap='20px'>
+                                <Box display='flex' w='100%' gap='20px'>
+                                    <Box w='50%' display='flex' flexDirection='column' gap='20px'>
+                                        <FormControl>
+                                            <FormLabel color='#8FB381'>Product Image</FormLabel>
+                                            <Box mb={"25px"} objectFit={"contain"} bgColor='#EEEDED' h='265px' display='flex' alignItems='center' justifyContent='center' borderRadius='10px'>
+                                                <Box {...getRootProps()} className="dropzone" objectFit={"contain"} color="#ffffff" display="flex" alignItems="center" justifyContent="center">
+                                                    <Input {...getInputProps()} size="xl" type='file' w='100%' h='100%' position='absolute' opacity='0' />
+                                                    <Box hidden={image ? true : false}>
+                                                        <IconPhotoDown color='#132043' width='130px' height='80px' />
+                                                    </Box>
+                                                </Box>
+                                                {image && <img src={image}/>}
+                                            </Box>
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel color='#8FB381'>Status</FormLabel>
+                                            <Select bgColor='#EEEDED' placeholder='Select Status' name="statusId" value={formik.values.statusId} onChange={formik.handleChange}>
+                                                {status?.map((item, index) => (
+                                                    <option key={index} value={item.id}>{item.statusName}</option>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Box>
+                                    <Box w='50%' display='flex' flexDirection='column' gap='41px'>
+                                        <FormControl>
+                                            <FormLabel color='#8FB381'>Product Name</FormLabel>
+                                            <Input bgColor='#EEEDED' placeholder='Product name here...' name="productName" value={formik.values.productName} onChange={formik.handleChange} />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel color='#8FB381'>Price</FormLabel>
+                                            <InputGroup>
+                                                <InputLeftAddon bgColor='#EEEDED' children='Rp' />
+                                                <Input type='tel' bgColor='#EEEDED' placeholder='Price' name="price" value={formik.values.price} onChange={formik.handleChange} />
+                                            </InputGroup>
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel color='#8FB381'>Stock</FormLabel>
+                                            <Input bgColor='#EEEDED' placeholder='Total Stock' name="stock" value={formik.values.stock} onChange={formik.handleChange} />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel color='#8FB381'>Product Category</FormLabel>
+                                            <Select bgColor='#EEEDED' placeholder='Select Category' name="categoryId" value={formik.values.categoryId} onChange={formik.handleChange}>
+                                                {category?.map((item, index) => (
+                                                    <option key={index} value={item.id}>{item.categoryName}</option>
+                                                ))}
+                                            </Select>
+                                        </FormControl>
+                                    </Box>
+
+                                </Box>
+                                <FormControl>
+                                    <FormLabel color='#8FB381'>Description</FormLabel>
+                                    <Textarea bgColor='#EEEDED' placeholder='Tell the description of the product here...' name="description" value={formik.values.description} onChange={formik.handleChange} />
+                                </FormControl>
+                            </Box>
+                        </ModalBody>
+                        <ModalFooter display='flex' gap='20px'>
+                            <Button w='80px' variant='outline' color='#8FB381' border='1px solid #132043' type='submit'>Save</Button>
+                            <Button w='80px' bgColor='#ffffff' color='#8FB381' onClick={handleResetForm}>Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </form>
+            </Modal>
+        </Box>
+    );
+}
+
+export default ModalCreateProduct;
