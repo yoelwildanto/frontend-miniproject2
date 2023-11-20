488fab45603e138591043a88d3b4e175e1d3978f
yoelwildanto <yoel.wildantop@gmail.com>
2023-11-20 09:45:42 +0700

    add product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import AdminPage from './pages/AdminView';
+import HeaderManageProduct from './components/manageProduct/headerManageProduct';
 import "./App.css"
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
-    <div className="App">
-     <AdminPage/>
+    <div>
+      <Routes>
+        <Route path='/' element={<AdminPage />} />
+        <Route path='/manage-product' element={<AdminPage viewHeader={<HeaderManageProduct />} />} />
+      </Routes>
     </div>
   );
 }
