aec1325838bd51b3725ee3489908117c39de245a
yoelwildanto <yoel.wildantop@gmail.com>
2023-11-20 13:05:08 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import AdminPage from './pages/AdminView';
 import HeaderManageProduct from './components/manageProduct/headerManageProduct';
 import BodyManageProduct from './components/manageProduct/bodyManageProduct';
@@ -6,11 +7,28 @@ import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [inputSearch, setInputSearch] = useState("");
+
+  const handlePageChange = (newPage, event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    setCurrentPage(newPage);
+  }
+
+  const handleSearch = (productName, event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    setInputSearch(productName);
+  };
   return (
     <div>
       <Routes>
         <Route path='/' element={<AdminPage />} />
         <Route path='/manage-product' element={<AdminPage viewHeader={<HeaderManageProduct/>} viewBody={<BodyManageProduct/>} />} />
+        <Route path='/product-list' element={<AdminPage viewHeader={<HeaderProductList inputSearch={handleSearch} />} viewBody={<BodyProductList currentPage={currentPage} onPageChange={handlePageChange} inputSearch={inputSearch}/>} viewFooter={<FooterProductList currentPage={currentPage} onPageChange={handlePageChange}/>}/>} />
       </Routes>
     </div>
   );
