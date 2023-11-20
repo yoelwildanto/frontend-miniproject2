2703bbeb13816f79b4882acf9f0c5d14a5494bfa
yoelwildanto <yoel.wildantop@gmail.com>
2023-11-20 22:38:47 +0700

    delete search on manage product

--- src/components/manageProduct/headerManageProduct.js
+++ src/components/manageProduct/headerManageProduct.js
@@ -9,10 +9,10 @@ const HeaderManageProduct  = () => {
             <Box display='flex' alignItems='center' justifyContent='space-between'>
                 <Text color='#000000' fontFamily="Poppins" fontWeight='700' fontSize='20px'>Manage Product</Text>
                 <Box display='flex' alignItems='center' gap='18px'>
-                    <Box w='375px' display='flex' alignItems='center' bgColor='#FFFFFF' padding='0 5px 0 5px' borderRadius='5px'>
+                    {/* <Box w='375px' display='flex' alignItems='center' bgColor='#FFFFFF' padding='0 5px 0 5px' borderRadius='5px'>
                         <Input border='none' _focus={{border: "none", boxShadow: "none",}} placeholder='Search Product here....'/>
                         <IconSearch color='#838383' />
-                    </Box>
+                    </Box> */}
                     
                     <Button bgColor='#132043' color='#ffffff' display='flex' gap='10px' onClick={onOpen}> <IconPlus/> <Text>Create Product</Text> </Button>
                 </Box>
